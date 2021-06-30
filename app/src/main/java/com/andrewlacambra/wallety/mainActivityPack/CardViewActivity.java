23676add616ac264502889485a647ac2f25a6d68
IbuosJanYuri <84585623+IbuosJanYuri@users.noreply.github.com>
2021-06-30 11:29:28 +0800

    Files upload

---
+++ app/src/main/java/com/andrewlacambra/wallety/mainActivityPack/CardViewActivity.java
@@ -0,0 +1,22 @@
+package com.andrewlacambra.wallety.mainActivityPack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+
+import com.andrewlacambra.wallety.R;
+
+/**
+ * CardView is our item's card
+ */
+
+public class CardViewActivity extends AppCompatActivity {
+    CardView mCardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCardView = findViewById(R.id.card_view);
+        setContentView(mCardView);
+    }
+}
